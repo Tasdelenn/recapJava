1a520caa1d82c24fe9679a3bb4237fde253969c0
HaQaN <hakann.tasdelenn@gmail.com>
2022-09-26 13:55:35 +0300

    OOP Concept Metodlarla Çalışma

---
+++ src/OOP2Odev/_04_Demo_Classes/DortIslem.java
@@ -0,0 +1,22 @@
+package OOP2Odev._04_Demo_Classes;
+
+public class DortIslem {
+    //Basit Hesap Makinesi
+
+    public int sum(int formalNum1, int formalNum2){
+        return formalNum1+formalNum2;
+    }
+
+    public int minus(int formalNum1, int formalNum2){
+        return formalNum1-formalNum2;
+    }
+
+    public int multiply(int formalNum1, int formalNum2){
+        return formalNum1*formalNum2;
+    }
+
+    public double devide(int formalNum1, int formalNum2){
+        return (double) formalNum1/formalNum2;
+    }
+
+}

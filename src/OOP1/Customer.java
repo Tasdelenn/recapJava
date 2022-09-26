f6acde705f7a01978645db22e691127d5a3d1e3c
HaQaN <hakann.tasdelenn@gmail.com>
2022-09-26 13:38:22 +0300

    OOP Temel Basic Operations / Müşteri Sınıfı Oluşturuldu

---
+++ src/OOP1/Customer.java
@@ -0,0 +1,35 @@
+package OOP1;
+
+public class Customer {
+
+    private int id;
+    private String customerNumber;
+    private String phone;
+
+    //private yaptık ya, getter setter lazım:
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCustomerNumber() {
+        return customerNumber;
+    }
+
+    public void setCustomerNumber(String customerNumber) {
+        this.customerNumber = customerNumber;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}

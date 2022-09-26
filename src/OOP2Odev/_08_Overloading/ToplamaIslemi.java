7fe2c0db6e111c9fb65cb3aa387ae3e1fb97a499
HaQaN <hakann.tasdelenn@gmail.com>
2022-09-26 14:01:27 +0300

    Overloading (Metodun aşırı yüklenmesi) Aynı isimli metodu, farklı parametre sayılarıyla kullanmak.

---
+++ src/OOP2Odev/_08_Overloading/ToplamaIslemi.java
@@ -0,0 +1,22 @@
+package OOP2Odev._08_Overloading;
+
+public class ToplamaIslemi {
+
+    public int topla(int num1, int num2){
+        return num1+num2;
+    }
+
+    public int topla(int num1, int num2, int num3){
+        return num1+num2+num3;
+    }
+
+    public int topla(int num1, int num2, int num3, int num4){
+        return num1+num2+num3+num4;
+    }
+
+    public String topla(int num1, int num2, int num3, String formalStr){ // Bu şekilde henüz çalıştıramadım.
+        int topla = num1+num2+num3;
+        return (String.valueOf(topla) + " " + formalStr);
+    }
+
+}

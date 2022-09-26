f49de96ee6f9a311f2b93f8660a786f0fe54db4b
HaQaN <hakann.tasdelenn@gmail.com>
2022-09-26 13:30:39 +0300

    for Basic

---
+++ src/Intro/_05_forBasic.java
@@ -0,0 +1,14 @@
+package Intro;
+
+public class _05_forBasic {
+
+    public static void main(String[] args) {
+
+        // for
+
+        for (int i = 0; i < 10; i+=2) {
+            System.out.println("i = " + i);
+        }
+        System.out.println("The loop is over!");
+    }
+}

c62ca3121e449140c92890af73ae48672944929f
HaQaN <hakann.tasdelenn@gmail.com>
2022-09-26 13:54:21 +0300

    OOP concept devam...

---
+++ src/OOP2Odev/_03_Classes/CustomerManager.java
@@ -0,0 +1,17 @@
+package OOP2Odev._03_Classes;
+
+public class CustomerManager {
+
+    public void add(){
+        System.out.println("Müşteri eklendi");
+    }
+
+    public void remove(){
+        System.out.println("Müşteri silindi");
+    }
+
+    public void update(){
+        System.out.println("Müşteri güncellendi");
+    }
+
+}

944db122ae95f4d3053015a04ff95b84a9bd3c06
HaQaN <hakann.tasdelenn@gmail.com>
2022-10-11 18:29:14 +0300

    Product Manager sınıfımızda İş kuralları tanımlayabiliriz Böylece, burada oluşturduğumuz nesneler (object) bu kurallara uygun bi şekilde oluşur.

--- src/OOP3withNLayeredApp/Business/ProductManager.java
+++ src/OOP3withNLayeredApp/Business/ProductManager.java
@@ -1,4 +1,17 @@
 package OOP3withNLayeredApp.Business;
 
+import OOP3withNLayeredApp.DataAccess.JdbcProductDao;
+import OOP3withNLayeredApp.Entities.Product;
+
 public class ProductManager {
+
+    public void add(Product product) throws Exception {
+        // İş kuralları buraya yazılır.
+        if (product.getUnitPrice() < 10) {
+            throw new Exception("Ürün fiyatı 10 TL 'den küçük olamaz");
+        }
+
+        JdbcProductDao productDao = new JdbcProductDao();
+        productDao.add(product);
+    }
 }

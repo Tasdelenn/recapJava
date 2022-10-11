e4976c2913a451461ceedd9678bb22f6177597b0
HaQaN <hakann.tasdelenn@gmail.com>
2022-10-11 18:20:24 +0300

    Ürün ekleme (ADD) ilişkin kurallar başka bi sınıfta tanımlanır: Business >> İş kuralları...

--- src/OOP3withNLayeredApp/DataAccess/JdbcProductDao.java
+++ src/OOP3withNLayeredApp/DataAccess/JdbcProductDao.java
@@ -3,8 +3,13 @@ package OOP3withNLayeredApp.DataAccess;
 import OOP3withNLayeredApp.Entities.Product;
 
 public class JdbcProductDao {
+
     public void add(Product product){
-        //SAdece ve sadece DB erişim kodları yazılır buraya (Çünkü Data Access kısmındayız) >> SQL
+        //Sadece ve sadece DB erişim kodları yazılır buraya (Çünkü Data Access kısmındayız) >> SQL
         System.out.println("JDBC ile veritabanına eklendi");
     }
+    // Mesela ürünlere bi telefon eklemek istiyoruz. Ama mesela bu ürün duplicate olmamalı.
+    // O yüzden buna ilişkin bi kuralımız olacak. Ama yeri burası değil.
+    // Sonuçta kurallar işe ilişkin, ürüne özgü değil.
+    //O yüzden kuralları Business kısmında tanımlayacağız.
 }

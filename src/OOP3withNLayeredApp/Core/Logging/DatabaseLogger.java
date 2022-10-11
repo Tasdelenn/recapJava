497078289f4907a1111729ef90f1365ef57d85de
HaQaN <hakann.tasdelenn@gmail.com>
2022-10-11 22:24:06 +0300

    2. Yönetim diyo ki veritabanındaki değişiklikleri de logla...

---
+++ src/OOP3withNLayeredApp/Core/Logging/DatabaseLogger.java
@@ -0,0 +1,8 @@
+package OOP3withNLayeredApp.Core.Logging;
+
+public class DatabaseLogger implements ILogger{
+    @Override
+    public void log(String data) {
+        System.out.println("Veritabanına loglandı: " + data);
+    }
+}

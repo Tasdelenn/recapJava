137b2e1ad3e433288dd4b95f137b571b6ff81db9
HaQaN <hakann.tasdelenn@gmail.com>
2022-10-11 18:41:48 +0300

    Main'in User Interface ekranı olduğunu farz edelim. 1. Müşteri ekrandan ürün girdi 2. Girilen ürün iş kurallarına uyumlu mu? Kontrol edildi (productManager >> throw exception) 3. İş kurallarına uygunsa veritabanına eklendi...

--- src/OOP3withNLayeredApp/Main.java
+++ src/OOP3withNLayeredApp/Main.java
@@ -1,9 +1,18 @@
 package OOP3withNLayeredApp;
 
+import OOP3withNLayeredApp.Business.ProductManager;
+import OOP3withNLayeredApp.Entities.Product;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        // UI (Arayüzümüz de Main olsun... Yani müşterinin ekrandan girişlerini buradan yaptığını farz edelim.)
+        // Şimdilik arada API olmadığını varsayarsak, arayüz (UI) iş birimiyle kontak kurar...
 
+        Product product1 = new Product(1,"Iphone 13Pro",20000); // Normalde müşteri ID girmez de... Öğrenmek için varsayım yapıyoruz...
+
+        ProductManager productManager = new ProductManager();
+        productManager.add(product1);
 
     }
 }

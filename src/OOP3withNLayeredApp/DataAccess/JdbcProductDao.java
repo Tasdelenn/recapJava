f3aa7830ad03b3673982457fce124a190bd14ca2
HaQaN <hakann.tasdelenn@gmail.com>
2022-10-11 18:15:09 +0300

    add metodunu Product için yapalım: *Entities.Product sınıfı projeye import edilmiş oldu. *Data Access le alakalı işlem yapacağımız için, metoda sadece DB erişim kodları (SQL) yazılır.

--- src/OOP3withNLayeredApp/DataAccess/JdbcProductDao.java
+++ src/OOP3withNLayeredApp/DataAccess/JdbcProductDao.java
@@ -4,6 +4,7 @@ import OOP3withNLayeredApp.Entities.Product;
 
 public class JdbcProductDao {
     public void add(Product product){
+        //SAdece ve sadece DB erişim kodları yazılır buraya (Çünkü Data Access kısmındayız) >> SQL
         System.out.println("JDBC ile veritabanına eklendi");
     }
 }

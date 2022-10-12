f48a060748d6e85887a02bc13a0b845955ccb279
HaQaN <hakann.tasdelenn@gmail.com>
2022-10-12 15:45:06 +0300

    Polimorfizm ve Injection *Artık üç classımız da BaseLogger 'i extend ettiği için, Base sınıf bunların (DatabaseLogger,FileLogger,EmailLogger) üçünün referansını da tutabilir hale geldi. *Hatta bir BaseLogger dizisi tanımlayıp içine istediğimiz sınıfı atabilir hale geldik (INJECTION). *İstersek yeni bir logger sınıfı da tanımlayabilir (mesela ConsoleLogger) veya listeden istediğimiz sınıfı silebiliriz... Tüm bunlar kodun diğer kısımlarını etkilememiş olur.

--- src/OOP_2_3_Odev/_11_PolymorphismDemo/Main.java
+++ src/OOP_2_3_Odev/_11_PolymorphismDemo/Main.java
@@ -3,6 +3,23 @@ package OOP_2_3_Odev._11_PolymorphismDemo;
 public class Main {
     public static void main(String[] args) {
 
+        EmailLogger logger = new EmailLogger();
+        logger.Log("Log mesajı");
+
+        DatabaseLogger logger2 = new DatabaseLogger();
+        logger2.Log("Log mesajı 2");
+
+        System.out.println("*******************************");
+
+        BaseLogger[] loggers = new BaseLogger[]{new DatabaseLogger(), new EmailLogger(), new FileLogger()}; // injection
+        // BaseLogger kendisini extend eden sınıfların referansını tutabilir (Polimorfizm)
+        // FileLogger, DatabaseLogger, EmailLogger "BaseLogger" ı tutamaz ama, BaseLogger hepsini tutabilir.
+        // Bu şekilde loggers' a istediğimizi enjekte edebilir; istediğimizi silebliriz. Kod bloğunun geri kalanı etkilenmez.
+
+        for (BaseLogger log : loggers){
+            log.Log("Log mesajı");
+        }
+
 
     }
 }

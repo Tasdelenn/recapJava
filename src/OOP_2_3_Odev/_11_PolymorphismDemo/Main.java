d21257469985f99c81747263faeb008487d1d055
HaQaN <hakann.tasdelenn@gmail.com>
2022-10-12 15:54:26 +0300

    BaseLogger[] loggers Dizideki emailLogger ı çıkarıp, tanımladığımız consoleLogger 'ı diziye enjekte ettik.'

--- src/OOP_2_3_Odev/_11_PolymorphismDemo/Main.java
+++ src/OOP_2_3_Odev/_11_PolymorphismDemo/Main.java
@@ -11,10 +11,11 @@ public class Main {
 
         System.out.println("*******************************");
 
-        BaseLogger[] loggers = new BaseLogger[]{new DatabaseLogger(), new EmailLogger(), new FileLogger()}; // injection
+        BaseLogger[] loggers = new BaseLogger[]{new DatabaseLogger(), new ConsoleLogger(), new FileLogger()}; // injection
         // BaseLogger kendisini extend eden sınıfların referansını tutabilir (Polimorfizm)
         // FileLogger, DatabaseLogger, EmailLogger "BaseLogger" ı tutamaz ama, BaseLogger hepsini tutabilir.
         // Bu şekilde loggers' a istediğimizi enjekte edebilir; istediğimizi silebliriz. Kod bloğunun geri kalanı etkilenmez.
+        // Mesela EmailLogger 'ı sildik; ConsoleLogger 'ı ekledik (Injection)... Kod blaoğunun kalan kısmı değişmedi...
 
         for (BaseLogger log : loggers){
             log.Log("Log mesajı");

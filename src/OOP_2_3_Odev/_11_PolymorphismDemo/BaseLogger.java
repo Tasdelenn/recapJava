f0733273cb4311f75341e0b62b064e08679c125b
HaQaN <hakann.tasdelenn@gmail.com>
2022-10-12 15:36:53 +0300

    Loglama için Base class oluşturduk

--- src/OOP_2_3_Odev/_11_PolymorphismDemo/BaseLogger.java
+++ src/OOP_2_3_Odev/_11_PolymorphismDemo/BaseLogger.java
@@ -2,7 +2,7 @@ package OOP_2_3_Odev._11_PolymorphismDemo;
 
 public class BaseLogger {
     public void Log(String message){
-
+        System.out.println("Default logger : " + message);
     }
 
 }

02ca4704186de7fa0d61a9e17f06ce8fd0e7a4fe
HaQaN <hakann.tasdelenn@gmail.com>
2022-10-12 17:25:53 +0300

    CustomerManager CustomerManager sınıfı sadece müşteri eklemek ve logunu görmek istiyo. BaseLogger sınıfını diğer 4 sınıf extend ettiği için, constructor sırasında, bu 4 sınıftan herhangi birini new leyebiliriz (injection).

---
+++ src/OOP_2_3_Odev/_11_PolymorphismDemo/CustomerManager.java
@@ -0,0 +1,18 @@
+package OOP_2_3_Odev._11_PolymorphismDemo;
+
+public class CustomerManager extends BaseLogger {
+    public BaseLogger logger; // 1. field oluşturdum.
+
+    public CustomerManager(BaseLogger logger) {     // 2. constructor oluşturdum, new lenirken burası çalışacak.
+        this.logger = logger;                       // ve new lenirken bana bi BaseLogger verecek (logger)
+    }
+
+    public void add() {     // Diğer logger larda Base 'i extend ettiğine göre artık add metodunu istediğim logger la çalıştırabilirim...
+
+        System.out.println("Müşteri eklendi");
+        this.logger.Log("Log mesajı");
+
+        // add yapıldığında, hem müşteri eklendi yazacak; hem de hangi sınıfla new leme yapıldıysa, constructor ı çalışacak ve onun log mesajını da yazacak...
+
+    }
+}

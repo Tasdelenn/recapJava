ece5801d62ed8ff906fc85e3bd6d47444b848b12
HaQaN <hakann.tasdelenn@gmail.com>
2022-10-13 10:46:35 +0300

    Main class ve açıklama

---
+++ src/OOP_2_3_Odev/_15_Interfaces/Main.java
@@ -0,0 +1,9 @@
+package OOP_2_3_Odev._15_Interfaces;
+
+public class Main {
+    public static void main(String[] args) {
+        /**
+         * İş katmanı (CustomerManager) MySQL veya Oracle'ı kullansın...
+         */
+    }
+}

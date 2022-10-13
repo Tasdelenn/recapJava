541626002c90c7b54ed9ab26e1ae3119ad8a0fb5
HaQaN <hakann.tasdelenn@gmail.com>
2022-10-13 11:24:51 +0300

    Main class ve açıklama...

---
+++ src/OOP_2_3_Odev/_16_InterfaceDemo/Main.java
@@ -0,0 +1,13 @@
+package OOP_2_3_Odev._16_InterfaceDemo;
+
+public class Main {
+    public static void main(String[] args) {
+        /**
+         * Bir bankanın yazılım departmanında kendi personelleri dışında bir de dışarıdan görevlendirilen personeller
+         * olduğunu (outsource worker) düşünelim...
+         * Doğal olarak, bu personellerin ortak operasyonları da olacaktır, benzer operasyonları da olacaktır.
+         * Interface demo için bu senaryoyu kullanacağız... Çoklu implementasyonlar gerçekleştireceğiz.
+         */
+
+    }
+}

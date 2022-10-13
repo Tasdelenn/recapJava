8ca9b9a3412ea4f3ae181e257345bb08e7ab8893
HaQaN <hakann.tasdelenn@gmail.com>
2022-10-13 10:41:47 +0300

    Package ismi güncellendi

--- src/OOP_2_3_Odev/_14_AbstractDemo/BaseDatabaseManager.java
+++ src/OOP_2_3_Odev/_14_AbstractDemo/BaseDatabaseManager.java
@@ -1,4 +1,4 @@
-package OOP_2_3_Odev.AbstractDemo;
+package OOP_2_3_Odev._14_AbstractDemo;
 
 public abstract class BaseDatabaseManager {
 
--- src/OOP_2_3_Odev/_14_AbstractDemo/CustomerManager.java
+++ src/OOP_2_3_Odev/_14_AbstractDemo/CustomerManager.java
@@ -1,4 +1,4 @@
-package OOP_2_3_Odev.AbstractDemo;
+package OOP_2_3_Odev._14_AbstractDemo;
 
 public class CustomerManager {
     BaseDatabaseManager databaseManager;   // Bizim stratejik nesnemiz. Çünkü ister Oracle'ı ister SQL Server'i

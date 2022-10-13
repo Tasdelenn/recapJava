39bccdf61199337b80b86eb235a8bc9cb8e64c82
HaQaN <hakann.tasdelenn@gmail.com>
2022-10-13 09:59:00 +0300

    Maain class ve açıklamalar

---
+++ src/OOP_2_3_Odev/AbstractDemo/Main.java
@@ -0,0 +1,12 @@
+package OOP_2_3_Odev.AbstractDemo;
+
+public class Main {
+    public static void main(String[] args) {
+        /**
+         * Bir müşterinin veritabanına kaydedilmesi örneğini yapacağız.
+         * Bunu mySQL, Oracle, MS SQL, PostGRE SQL... gibi farklı veritabanı programıyla yapabiliriz.
+         *
+         */
+
+    }
+}

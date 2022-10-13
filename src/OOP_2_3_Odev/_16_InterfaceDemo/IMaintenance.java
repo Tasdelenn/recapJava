1c845565b3a97be00bbfec391cdc7c0897491646
HaQaN <hakann.tasdelenn@gmail.com>
2022-10-13 11:37:44 +0300

    Şirkete robotlar alınmış olsun...

---
+++ src/OOP_2_3_Odev/_16_InterfaceDemo/IMaintenance.java
@@ -0,0 +1,5 @@
+package OOP_2_3_Odev._16_InterfaceDemo;
+
+public interface IMaintenance {
+    void maintanence();
+}
---
+++ src/OOP_2_3_Odev/_16_InterfaceDemo/Robot.java
@@ -0,0 +1,13 @@
+package OOP_2_3_Odev._16_InterfaceDemo;
+
+public class Robot implements IWorker,IMaintenance{
+    @Override
+    public void work() {
+
+    }
+
+    @Override
+    public void maintanence() {
+
+    }
+}

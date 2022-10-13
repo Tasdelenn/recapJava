087b3e88ff4ef2a61126dc4bc1d4b7ee28375723
HaQaN <hakann.tasdelenn@gmail.com>
2022-10-13 07:25:01 +0300

    2. aşama Ogretmen ve Tarım için Basit kredi tutarı hesaplama dizisi oluşturuldu

--- src/OOP_2_3_Odev/Overriding/Main.java
+++ src/OOP_2_3_Odev/Overriding/Main.java
@@ -3,8 +3,16 @@ package OOP_2_3_Odev.Overriding;
 public class Main {
     public static void main(String[] args) {
 
-        OgretmenCreditManager ogretmenKredisi = new OgretmenCreditManager();
-        System.out.println("ogretmenKredisi tutar = " + ogretmenKredisi.hesapla(1000));
+        BaseCreditManager[] creditManagers = new BaseCreditManager[]
+                {new OgretmenCreditManager(),new TarimCreditManager()};
+        //Ogretmen ve Tarım kredi ikisi de Base class'ı extend ettiği için BaseClass nesnesi,
+        //ikisinin de referansını tutabiliyo, hatta aynı dizinin içine yazbiliyoruz...
+
+        for (BaseCreditManager kredi: creditManagers) {
+            System.out.println("kredi = " + kredi.hesapla(2000));
+        }
+        //Şu anda ikisi için de aynı tutarı hesapladı.... Ama mesela banka diyelim ki,
+        //farklı oranlarla yeni bi öğrenci kredisi vermek istedi... Bunu nasıl yapacağız?
 
     }
 }

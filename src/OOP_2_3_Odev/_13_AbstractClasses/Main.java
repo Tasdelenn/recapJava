b6aac1beb8af0bf4f356bd7cf82900c17a90a7bc
HaQaN <hakann.tasdelenn@gmail.com>
2022-10-13 07:54:12 +0300

    Main class ve açıklamalar

---
+++ src/OOP_2_3_Odev/_13_AbstractClasses/Main.java
@@ -0,0 +1,16 @@
+package OOP_2_3_Odev._13_AbstractClasses;
+
+public class Main {
+    public static void main(String[] args) {
+        /**
+         * Bir oyun için puan hesaplanmak isteniyo. Oyunda Erkek, Kadın ve Çocuk oyuncular
+         * için farklı hesaplama yöntemi kullanılacak...
+         */
+
+        // Bunu yaparken, if erkek ise; if kadın ise; if çocuk ise... gibi bir yöntemden
+        // uzak duruyoruz. Yoksa kodumuz spagetti koda döner.
+        // Sonuçta hepsinde bir hesaplama yöntemi olacağı için aklımıza hemen BaseHesaplama Class'ı gelecek!
+
+
+    }
+}

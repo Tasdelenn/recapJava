9dbc269698813fb10a7997adc91e3c4be093cae2
HaQaN <hakann.tasdelenn@gmail.com>
2022-10-13 10:22:04 +0300

    Müşteri listesini database den getirmek istiyoruz DOĞRU KULLANIM

--- src/OOP_2_3_Odev/AbstractDemo/CustomerManager.java
+++ src/OOP_2_3_Odev/AbstractDemo/CustomerManager.java
@@ -1,15 +1,16 @@
 package OOP_2_3_Odev.AbstractDemo;
 
 public class CustomerManager {
+    BaseDatabaseManager databaseManager;   // Bizim stratejik nesnemiz. Çünkü ister Oracle'ı ister SQL Server'i
+                                           // İstersek de sonradan oluşturabileceğimiz mesela bir MySQL nesnesinin
+                                           // REFERANSLARINI TUTABİLİR
 
     public void getCustomer(){  // Müşterileri getir diye bir operasyon yazıyorum.
                                 // Sonuçta bunun için ya Oracle ya da SQL Server 'ı kullanıcam.
                                 // Şayet Oracle ya da SQL Server nesnesini burada oluşturduğumuz an o nesneye bağımlı hale geliriz...
                                 // Bunu bağımlı olmadan yapmaya çalışıcaz.
 
-        /** Yanlış Kullanım: */
-        OracleDatabaseManager oracleDatabaseManager1 = new OracleDatabaseManager();
-        oracleDatabaseManager1.getData();
-        /** ORACLE a bağımlı hale gelmiş oluruz... */
+        databaseManager.getData();  // DOĞRU KULLANIM
+
     }
 }
